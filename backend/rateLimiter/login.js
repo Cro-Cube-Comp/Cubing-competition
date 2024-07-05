4fa5a64cf299a2e529c7dd2ef04dd6f6fe9c46ac
Jakov <qwerasdftzu@gmail.com>
2024-07-05 13:36:00 +0200

    Make disabling rate limiting work for login and register route

--- backend/rateLimiter/login.js
+++ backend/rateLimiter/login.js
@@ -1,8 +1,11 @@
 const rateLimit = require("express-rate-limit");
+const { isRateLimitingEnabled } = require("../functions/options");
 // Specific rate limit for the login route
-const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 15, // limit each IP to 15 login requests per windowMs
-  message: "Previše pokušaja prijave, pokušajte ponovno za 15 minuta.",
-});
+const loginLimiter = isRateLimitingEnabled
+  ? rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 15, // limit each IP to 15 login requests per windowMs
+      message: "Previše pokušaja prijave, pokušajte ponovno za 15 minuta.",
+    })
+  : (req, res, next) => next();
 module.exports = loginLimiter;
--- backend/rateLimiter/register.js
+++ backend/rateLimiter/register.js
@@ -1,8 +1,9 @@
 const rateLimit = require("express-rate-limit");
+const { isRateLimitingEnabled } = require("../functions/options");
 // Specific rate limit for the login route
-const registerLimiter = rateLimit({
+const registerLimiter = isRateLimitingEnabled ? rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 5, // limit each IP to 5 login requests per windowMs
   message: "Too many register attempts, please try again after 15 minutes",
-});
+}) : (req, res, next) => next();
 module.exports = registerLimiter;
