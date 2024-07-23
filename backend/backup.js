d2401ec3fedb1f2ec0b9166b4ff5d09ec7a94c80
Jakov <qwerasdftzu@gmail.com>
2024-07-23 20:24:16 +0200

    Cleaner code

--- backend/backup.js
+++ backend/backup.js
@@ -6,36 +6,28 @@ import { join } from "path";
 
 config();
 
-console.log(`Running ${import.meta.file}`);
-try {
+async function connectToDatabase() {
   console.time("Connect to mongodb");
-  await mongoose.connect(process.env.MONGO_URI);
-  console.timeEnd("Connect to mongodb");
-} catch (error) {
-  console.error("Failed to connect to MongoDB: \n" + error);
-  process.exit(1);
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.timeEnd("Connect to mongodb");
+    console.log("Connected to mongodb");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB: \n" + error);
+    process.exit(1);
+  }
 }
-console.log("Connected to mongodb");
-console.time("Backup");
-await main();
-console.timeEnd("Backup");
-console.log("DONE...");
-exit();
 
-async function main() {
+async function backupCollections() {
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
-  console.log("Connected to the database.");
 
   const collections = await db.db.listCollections().toArray();
   const backupDir = "backups";
 
-  // Create the backups directory if it doesn't exist
   await mkdir(backupDir, { recursive: true });
 
-  for (const collectionInfo of collections) {
-    const collectionName = collectionInfo.name;
+  for (const { name: collectionName } of collections) {
     const collection = db.collection(collectionName);
     const data = JSON.stringify(await collection.find({}).toArray());
 
@@ -43,13 +35,25 @@ async function main() {
       const filePath = join(backupDir, `${collectionName}-backup.json`);
       await writeFile(filePath, data);
       console.log(
-        `Backup of the '${collectionName}' collection saved successfully.`,
+        `Backup of the '${collectionName}' collection saved successfully.`
       );
     } catch (err) {
       console.error(
         `Error writing to file for collection '${collectionName}':`,
-        err,
+        err
       );
     }
   }
 }
+
+async function main() {
+  await connectToDatabase();
+  console.time("Backup");
+  await backupCollections();
+  console.timeEnd("Backup");
+  console.log("DONE...");
+  exit();
+}
+
+console.log(`Running ${import.meta.file}`);
+main();
