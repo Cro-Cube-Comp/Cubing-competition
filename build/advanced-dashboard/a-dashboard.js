460c84d9b47173e909b27e11e8a58282483675c3
Jakov <qwerasdftzu@gmail.com>
2024-07-24 21:29:06 +0200

    Make frontend for backups work

--- build/advanced-dashboard/a-dashboard.js
+++ build/advanced-dashboard/a-dashboard.js
@@ -17,12 +17,19 @@ getResultsBtn.addEventListener("click", getResults);
 function getResults() {
   getResultsBtn.disabled = true;
   const resultsUrl = addToken(
-    `${url}/results?competitionId=${compResultsSelect.value}`,
+    `${url}/results?competitionId=${compResultsSelect.value}`
   );
   downloadFile(resultsUrl, "results"); // You can specify the desired file name
   getResultsBtn.disabled = false;
 }
-
+const backupsBtn = document.querySelector(".backups");
+backupsBtn.addEventListener("click", getBackups);
+function getBackups() {
+  backupsBtn.disabled = true;
+  const backupsUrl = addToken(`${url}/backup`);
+  downloadFile(backupsUrl, "backups"); // You can specify the desired file name
+  backupsBtn.disabled = false;
+}
 async function changePassword(username, newPassword) {
   const body = {
     username,
@@ -40,7 +47,7 @@ async function changePassword(username, newPassword) {
   return response;
 }
 const changePasswordSubmitBtn = document.querySelector(
-  ".change-password-submit-btn",
+  ".change-password-submit-btn"
 );
 const newPasswordInput = document.querySelector(".new-password");
 const usernameInput = document.querySelector(".username");
