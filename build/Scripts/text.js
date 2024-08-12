7270cf51cafbbfebdc42a111f29a059553bbd66a
Jakov <qwerasdftzu@gmail.com>
2024-08-12 12:52:49 +0200

    header buttons work now, however selection does not work

--- build/Scripts/text.js
+++ build/Scripts/text.js
@@ -38,14 +38,37 @@ function headerText(text, start = 0, end = text.length, level) {
   const lines = text.split("\n");
 
   // Apply the header to each line if the entire line is within the range
+  let currentStart = 0;
+  let headeredLines = 0;
   const headerLines = lines.map((line, index) => {
-    const lineStart = text.indexOf(line);
+    const lineStart = currentStart;
     const lineEnd = lineStart + line.length;
+    currentStart += line.length + 1;
+
     if (line[0] === "#") {
-      // Line is a header already, so remove hashtags from start
-      return line.replace(/^#+\s*/, "");
+      // Line is a header already
+
+      // Get the number of hashtags at the start of the line
+      const hashtags = line.match(/^#+\s*/)[0].trim().length;
+
+      if (hashtags === level) {
+        // Line is already a header of the same level
+        // so remove hashtags from start
+        return line.replace(/^#+\s*/, "");
+      }
+      
+      // Change the hashtags to the desired level
+      return `${"#".repeat(level)} ${line.replace(/^#+\s*/, "")}`;
     }
-    if (lineStart <= start && lineEnd <= end) {
+    // make sure that only 1 line can be headered in 1 go
+    if (headeredLines >= 1) {
+      return line;
+    }
+    if (
+      (lineStart >= start && lineEnd <= end) ||
+      (start === end && lineStart <= start && lineEnd >= end)
+    ) {
+      headeredLines++;
       return header(line, level);
     }
     return line;
